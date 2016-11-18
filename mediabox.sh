4073d3826b58ac40f198b97aead7811ce0f31d38
Tom Morgan <tom472@gmail.com>
2016-11-18 16:36:39 -0500

    Get and echo the variables

--- mediabox.sh
+++ mediabox.sh
@@ -11,28 +11,40 @@ if [[ ! $EUID -eq 0 ]];then
 fi
 
 # Get local Username
-localuname=id -u -n
+localuname=`id -u -n`
 
 # Get PUID
-$PUID=id -u $localuname
+PUID=`id -u $localuname`
 
 # Get GUID
-$GUID=id -g $localuname
+GUID=`id -g $localuname`
 
 # Get Hostname
-thishost=$(hostname)
+thishost=`hostname`
 
 # Get IP Address
-locip=hostname -I | awk '{print $1}'
+locip=`hostname -I | awk '{print $1}'`
 
 # CIDR - this assumes a 255.255.255.0 netmask - If your config is different use the custom CIDR line
-lannet=hostname -I | awk '{print $1}' | sed 's/\.[0-9]*$/.0\/24/'
-# Custom CIDR (comment out the line above if using this) 
+lannet=`hostname -I | awk '{print $1}' | sed 's/\.[0-9]*$/.0\/24/'`
+# Custom CIDR (comment out the line above if using this)
 # Uncomment the line below and enter your CIDR info so the line looks like: lannet=xxx.xxx.xxx.0/24
 #lannet=
 
 # Install Docker
-$SUDO curl -fsSL https://get.docker.com/ | sh
+# $SUDO curl -fsSL https://get.docker.com/ | sh
 
 read -p "What is your PIA Username?: " piauname
 read -s -p "What is you PIA Password? (Will not be echoed): " piapass
+printf "\n\n"
+printf "### Collected Variable are below. ###\n"
+printf "\n"
+printf "The username is: $localuname\n"
+printf "The PUID is: $PUID\n"
+printf "The GUID is: $GUID\n"
+printf "The IP address is: $locip\n"
+printf "The CIDR address is: $lannet\n"
+printf "The PIA Username is: $piauname\n"
+printf "The PIA Password is: $piapass\n"
+printf "The Hostname is: $thishost\n"
+
