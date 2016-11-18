67838f17a0a60fe8540e1792ef181bbf55e07fc8
Tom Morgan <tom472@gmail.com>
2016-11-17 23:48:26 -0500

    Setting the variables

--- mediabox.sh
+++ mediabox.sh
@@ -10,31 +10,29 @@ if [[ ! $EUID -eq 0 ]];then
   fi
 fi
 
-# Capture Hostname
+# Get local Username
+localuname=id -u -n
+
+# Get PUID
+$PUID=id -u $localuname
+
+# Get GUID
+$GUID=id -g $localuname
+
+# Get Hostname
 thishost=$(hostname)
+
+# Get IP Address
+locip=hostname -I | awk '{print $1}'
+
+# CIDR - this assumes a 255.255.255.0 netmask - If your config is different use the custom CIDR line
+lannet=hostname -I | awk '{print $1}' | sed 's/\.[0-9]*$/.0\/24/'
+# Custom CIDR (comment out the line above if using this) 
+# Uncomment the line below and enter your CIDR info so the line looks like: lannet=xxx.xxx.xxx.0/24
+#lannet=
 
 # Install Docker
 $SUDO curl -fsSL https://get.docker.com/ | sh
 
-# Get base Mounted Volumes path for containers
-echo "What is the base path of the directory that you want to use for your mounted volumes : \n i.e. /dockervols"
-read -e BASEDIR
-$SUDO mkdir $BASEDIR
-
-if $rancher = true
-then
-$SUDO mkdir -p $BASEDIR/rancher
-read -p "What port do you want to use for Rancher? (Press enter for default 8080): " -e -i 8080 rancherip
-
-$SUDO mkdir -p $BASEDIR/delugevpn/data/complete
-$SUDO mkdir -p $BASEDIR/delugevpn/data/downloading
-$SUDO mkdir -p $BASEDIR/delugevpn/config
-read -p "What port do you want to use for DelugeVPN? (Press enter for default 8112): " -e -i 8112 delugevpnip
 read -p "What is your PIA Username?: " piauname
 read -s -p "What is you PIA Password? (Will not be echoed): " piapass
-read -p "We need your PIUD and GUID for this container .. What is your username on this PC?" localuname
-$PUID=id -u $localuname
-$GUID=id -g $localuname
-read -p "We need the CIDR network notation for your local network i.e. 192.168.0.1/24: " -e -i 192.168.0.1/24 lannet
-
-read -s -p "Password:" 
