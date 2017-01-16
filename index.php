669e8134987480f8b73a1161ee2d3d467e1d47cb
Tom Morgan <tom472@gmail.com>
2017-01-16 18:04:46 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+  font-family: "Open Sans", sans-serif;
+}
+pre {
+  font-size: 90%;
+  line-height: 1.2em;
+  font-family: "Courier 10 Pitch", Courier, monospace; 
+  white-space: pre; 
+  white-space: pre-wrap; 
+  white-space: -moz-pre-wrap; 
+  white-space: -o-pre-wrap; 
+
+  height:1%;
+  width: auto;
+  display: inline-block;
+  clear: both;
+  color: #555555;
+  padding: 1em 1em;
+  margin: auto 10px auto 10px;
+  background: #f4f4f4;
+  border: solid 1px #e1e1e1
+} 
+code { 
+  font-size: 90%;
+  line-height: 1.2em;
+  font-family: Monaco, Consolas, "Andale Mono", "DejaVu Sans Mono", monospace;
+
+  display: inline; 
+  color: #555555;
+  padding: 1em 1em;
+  background: #f4f4f4;
+}
+p {
+    border: 1px solid black;
+    display: inline-block;
+    padding: 5px
+}
+</style>
+</head>
+<body>
+<h1>Welcome to Mediabox!</h1>
+<h3>This page will help you manage your Mediabox server</h3>
+<b>Couchpotato:</b><br />
+As you go through the Couchpotato Setup Wizard use these settings:<br />
+<ol><li> Turn on Deluge -- Click the "slider" to the right of the Deluge option</ol><br />
+<ul><li> Host: locip:58846<br />
+<li> Username: daemonun<br />
+<li> Password: daemonpass<br />
+<li> Directory: [Leave Bliank]<br />
+<li> Label: [Leave Blank]<br />
+</ul>
+<br />
+After the Wizard is complete:<br />
+Click on the "gear" icon and select "Settings"<br />
+* Go to > Downloaders > Deluge > & click the "Test Deluge" button to verify the connection.<br />
+* Go to > Renamer - and turn it on > Click the "slider" to the right<br />
+# In the From field: /downloads<br />
+# In the To field: /movies<br />
+<br />
+<br />
+<p>Below are the variables in your .env file</p><br />
+<div><pre><?php include('../../../../../.env'); ?></pre></div>
+</body>
+</html>
