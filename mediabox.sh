f428de7c4711fb29e7f94cb19b3a8046d8ebd557
Tom <tom472@gmail.com>
2017-10-09 13:00:54 -0400

    Fixing some paths

--- mediabox.sh
+++ mediabox.sh
@@ -43,8 +43,8 @@ elif [ $portainerstyle == "auth" ]; then
 fi   
 
 # Create the directory structure
+`mkdir -p content/complete`
 `mkdir -p content/in_progress`
-`mkdir -p content/downloads`
 `mkdir -p content/movies`
 `mkdir -p content/tv`
 `mkdir -p couchpotato`
@@ -125,15 +125,6 @@ printf "Configuring Deluge daemon access - UHTTPD index file - Permissions \n\n"
 # Push the Deluge Daemon Access info the to Auth file
 `echo $daemonun:$daemonpass:10 >> ./delugevpn/config/auth`
 
-# Configure the DelugeVPN file paths, Set Daemon access on, delete the core.conf~ file
-`docker stop delugevpn > /dev/null 2>&1`
-`rm delugevpn/config/core.conf~ > /dev/null 2>&1`
-`sed -i 's/"allow_remote": false,/"allow_remote": true,/g'  delugevpn/config/core.conf`
-`sed -i 's/"\/home\/nobody\/Incompletes"/"\/data\/in_progress"/g' delugevpn/config/core.conf`
-`sed -i 's/"\/home\/nobody\/Completed"/"\/data\/downloads"/g' delugevpn/config/core.conf`
-`sed -i 's/"move_completed": false,/"move_completed": true,/g'  delugevpn/config/core.conf`
-`docker start delugevpn > /dev/null 2>&1`
-
 # Configure UHTTPD settings and Index file
 `docker stop uhttpd > /dev/null 2>&1`
 `mv index.html www/index.html` 
@@ -142,6 +133,15 @@ printf "Configuring Deluge daemon access - UHTTPD index file - Permissions \n\n"
 `sed -i "s/daemonpass/$daemonpass/g" www/index.html`
 `cp .env www/env.txt`
 `docker start uhttpd > /dev/null 2>&1`
+
+# Configure the DelugeVPN file paths, Set Daemon access on, delete the core.conf~ file
+`docker stop delugevpn > /dev/null 2>&1`
+`rm delugevpn/config/core.conf~ > /dev/null 2>&1`
+`sed -i 's/"allow_remote": false,/"allow_remote": true,/g'  delugevpn/config/core.conf`
+`sed -i 's/"\/home\/nobody\/Incompletes"/"\/data\/in_progress"/g' delugevpn/config/core.conf`
+`sed -i 's/"\/home\/nobody\/Completed"/"\/data\/complete"/g' delugevpn/config/core.conf`
+`sed -i 's/"move_completed": false,/"move_completed": true,/g'  delugevpn/config/core.conf`
+`docker start delugevpn > /dev/null 2>&1`
 
 # Adjust the permissions on the content folder
 `chmod -R 0777 content/`
