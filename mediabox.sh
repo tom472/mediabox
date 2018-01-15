bd73f100a1b1222d5e5f8ae3d80ed332c247a81a
Kyle Spillane <spillman@gmail.com>
2018-01-14 19:38:50 -0600

    Fixed lannet output to include CIDR bits calculated from the subnet mask

--- mediabox.sh
+++ mediabox.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#set -x
+
 # Get local Username
 localuname=`id -u -n`
 # Get PUID
@@ -27,6 +29,21 @@ IFS=. read -r m1 m2 m3 m4 <<< $subnet_mask
 IFS=$IFSold
 lannet=$(printf "%d.%d.%d.%d\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))")
 
+# Converts subnet mask into CIDR notation
+# thanks to https://stackoverflow.com/questions/20762575/explanation-of-convertor-of-cidr-to-netmask-in-linux-shell-netmask2cdir-and-cdir
+# because I kept messing it up.
+#
+# Define the function first, takes subnet as positional parameters
+function mask2cdr()
+{
+   # Assumes there's no "255." after a non-255 byte in the mask
+   local x=${1##*255.}
+   set -- 0^^^128^192^224^240^248^252^254^ $(( (${#1} - ${#x})*2 )) ${x%%.*}
+   x=${1%%$3*}
+   cidr_bits=$(( $2 + (${#x}/4) ))
+}
+mask2cdr $subnet_mask # Call the function to convert to CIDR
+lannet=$(echo "$lannet/$cidr_bits") # Combine lannet and cidr
 
 # Get Private Internet Access Info
 read -p "What is your PIA Username?: " piauname
