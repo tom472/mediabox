a2ef1000bb1844ca17be5f8e99bffef79aeae876
Ryan Williams <Switcher05@users.noreply.github.com>
2018-12-14 23:51:28 -0500

    Changes

--- mediabox.sh
+++ mediabox.sh
@@ -4,55 +4,53 @@
 
 # See if we need to check GIT for updates
 if [ -e .env ]; then
-    # Stash any local changes to the base files
-    git stash > /dev/null 2>&1
-    printf "Updating your local copy of Mediabox.\\n\\n"
-    # Pull the latest files from Git
-    git pull
-    # Check to see if this script "mediabox.sh" was updated and restart it if necessary
-    changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
-    check_run() {
-        echo "$changed_files" | grep --quiet "$1" && eval "$2"
-    }
-    # Provide a message once the Git check/update  is complete
+# Stash any local changes to the base files
+git stash > /dev/null 2>&1
+printf "Updating your local copy of Mediabox.\\n\\n"
+# Pull the latest files from Git
+git pull
+# Check to see if this script "mediabox.sh" was updated and restart it if necessary
+changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+check_run() {
+	echo "$changed_files" | grep --quiet "$1" && eval "$2"
+}
+# Provide a message once the Git check/update  is complete
     if [ -z "$changed_files" ]; then
-        printf "Your Mediabox is current - No Update needed.\\n\\n"
+    printf "Your Mediabox is current - No Update needed.\\n\\n"
     else
-        printf "Mediabox Files Update complete.\\n\\nThis script will restart if necessary\\n\\n"
+    printf "Mediabox Files Update complete.\\n\\nThis script will restart if necessary\\n\\n"
     fi
-    # Rename the .env file so this check fails if mediabox.sh needs to re-launch
-    mv .env 1.env
-    read -r -p "Press any key to continue... " -n1 -s
-    printf "\\n\\n"
-    # Run exec mediabox.sh if mediabox.sh changed
-    check_run mediabox.sh "exec ./mediabox.sh"
+# Rename the .env file so this check fails if mediabox.sh needs to re-launch
+mv .env 1.env
+read -r -p "Press any key to continue... " -n1 -s
+printf "\\n\\n"
+# Run exec mediabox.sh if mediabox.sh changed
+check_run mediabox.sh "exec ./mediabox.sh"
 fi
 
 # After update collect some current known variables
 if [ -e 1.env ]; then
-    # Grab the CouchPotato, NBZGet, & PIA usernames & passwords to reuse
-    daemonun=$(grep CPDAEMONUN 1.env | cut -d = -f2)
-    daemonpass=$(grep CPDAEMONPASS 1.env | cut -d = -f2)
-    piauname=$(grep PIAUNAME 1.env | cut -d = -f2)
-    piapass=$(grep PIAPASS 1.env | cut -d = -f2)
-    dldirectory=$(grep DLDIR 1.env | cut -d = -f2)
-    tvdirectory=$(grep TVDIR 1.env | cut -d = -f2)
-    moviedirectory=$(grep MOVIEDIR 1.env | cut -d = -f2)
-    musicdirectory=$(grep MUSICDIR 1.env | cut -d = -f2)
-    # Echo back the media directioies to see if changes are needed
-    printf "These are the Media Directory paths currently configured.\\n"
-    printf "Your DOWNLOAD Directory is: %s \\n" "$dldirectory"
-    printf "Your TV Directory is: %s \\n" "$tvdirectory"
-    printf "Your MOVIE Directory is: %s \\n" "$moviedirectory"
-    printf "Your MUSIC Directory is: %s \\n" "$musicdirectory"
-    read  -r -n 1 -p "Are these directiores still correct? (y/n) " diranswer
-    # Now we need ".env" to exist again so we can stop just the Medaibox containers
-    mv 1.env .env
-    # Stop the current Mediabox stack
-    printf "\\n\\nStopping Current Mediabox containers.\\n\\n"
-    docker-compose stop
-    # Make a datestampted copy of the existing .env file
-    mv .env "$(date +"%Y-%m-%d_%H:%M").env"
+# Grab the CouchPotato, NBZGet, & PIA usernames & passwords to reuse
+daemonun=$(grep CPDAEMONUN 1.env | cut -d = -f2)
+daemonpass=$(grep CPDAEMONPASS 1.env | cut -d = -f2)
+piauname=$(grep PIAUNAME 1.env | cut -d = -f2)
+piapass=$(grep PIAPASS 1.env | cut -d = -f2)
+tvdirectory=$(grep TVDIR 1.env | cut -d = -f2)
+moviedirectory=$(grep MOVIEDIR 1.env | cut -d = -f2)
+musicdirectory=$(grep MUSICDIR 1.env | cut -d = -f2)
+# Echo back the media directioies to see if changes are needed
+printf "These are the Media Directory paths currently configured.\\n"
+printf "TV Directory is: $tvdirectory \\n"
+printf "MOVIE Directory is: $moviedirectory \\n"
+printf "MUSIC Directory is: $musicdirectory \\n"
+read -n 1 -p "Are these directiores still correct? (y/n) " diranswer
+# Now we need ".env" to exist again so we can stop just the Medaibox containers
+mv 1.env .env
+# Stop the current Mediabox stack
+printf "\\n\\nStopping Current Mediabox containers.\\n\\n"
+docker-compose stop
+# Make a datestampted copy of the existing .env file
+mv .env "$(date +"%Y-%m-%d_%H:%M").env"
 fi
 
 # Get local Username
@@ -72,18 +70,18 @@ time_zone=$(cat /etc/timezone)
 # via @kspillane
 # Grab the subnet mask from ifconfig
 # Check Ubuntu version for output type
-ubunver=$(lsb_release -c | grep Codename | awk -F ' ' '{print $2}')
+ubunver=$(lsb_release -c | grep Codename | awk -F ' ' {'print $2'})
 if [ "$ubunver" == bionic ]; then
-    subnet_mask=$(ifconfig | grep "$locip" | awk -F ' ' '{print $4}')
+subnet_mask=$(ifconfig | grep $locip | awk -F ' ' {'print $4'})
 else
-    subnet_mask=$(ifconfig | grep "$locip" | awk -F ':' '{print $4}')
+subnet_mask=$(ifconfig | grep $locip | awk -F ':' {'print $4'})
 fi
 # Use bitwise & with ip and mask to calculate network address
 IFSold=$IFS
-IFS=. read -r i1 i2 i3 i4 <<< "$locip"
-IFS=. read -r m1 m2 m3 m4 <<< "$subnet_mask"
+IFS=. read -r i1 i2 i3 i4 <<< $locip
+IFS=. read -r m1 m2 m3 m4 <<< $subnet_mask
 IFS=$IFSold
-lannet=$(printf "%d.%d.%d.%d\\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))")
+lannet=$(printf "%d.%d.%d.%d\n" "$((i1 & m1))" "$((i2 & m2))" "$((i3 & m3))" "$((i4 & m4))")
 
 # Converts subnet mask into CIDR notation
 # Thanks to https://stackoverflow.com/questions/20762575/explanation-of-convertor-of-cidr-to-netmask-in-linux-shell-netmask2cdir-and-cdir
@@ -96,8 +94,8 @@ function mask2cdr()
    x=${1%%$3*}
    cidr_bits=$(( $2 + (${#x}/4) ))
 }
-mask2cdr "$subnet_mask" # Call the function to convert to CIDR
-lannet="$lannet/$cidr_bits" # Combine lannet and cidr
+mask2cdr $subnet_mask # Call the function to convert to CIDR
+lannet=$(echo "$lannet/$cidr_bits") # Combine lannet and cidr
 
 if [ -z "$piauname" ]; then
 # Get Private Internet Access Info
@@ -109,8 +107,8 @@ fi
 read -r -p "Which PLEX release do you want to run? By default 'public' will be used. (latest, public, plexpass): " pmstag
 read -r -p "If you have PLEXPASS what is your Claim Token from https://www.plex.tv/claim/ (Optional): " pmstoken
 # If not set - set PMS Tag to Public:
-if [ -z "$pmstag" ]; then
-   pmstag=public
+if [ -z "$pmstag" ]; then 
+   pmstag=public 
 fi
 
 # Get the info for the style of Portainer to use
@@ -120,8 +118,8 @@ if [ -z "$portainerstyle" ]; then
 elif [ "$portainerstyle" == "noauth" ]; then
    portainerstyle=--no-auth
 elif [ "$portainerstyle" == "auth" ]; then
-   portainerstyle=
-fi
+   portainerstyle= 
+fi   
 
 # Ask user if they already have TV, Movie, and Music directories
 if [ -z "$diranswer" ]; then
@@ -129,26 +127,16 @@ printf "\\n\\n"
 printf "If you already have TV - Movie - Music directories you want to use you can enter them next.\\n"
 printf "If you want Mediabox to generate it's own directories just press enter to these questions."
 printf "\\n\\n"
-read -r -p "Where do you store your DOWNLOADS? (Please use full path - /path/to/downloads ): " dldirectory
-read -r -p "Where do you store your TV media? (Please use full path - /path/to/tv ): " tvdirectory
-read -r -p "Where do you store your MOVIE media? (Please use full path - /path/to/movies ): " moviedirectory
-read -r -p "Where do you store your MUSIC media? (Please use full path - /path/to/music ): " musicdirectory
+read -r -p "Where do store your TV media? (Please use full path - /path/to/tv ): " tvdirectory
+read -r -p "Where do store your MOVIE media? (Please use full path - /path/to/movies ): " moviedirectory
+read -r -p "Where do store your MUSIC media? (Please use full path - /path/to/music ): " musicdirectory
 fi
 if [ "$diranswer" == "n" ]; then
-read -r -p "Where do you store your DOWNLOADS? (Please use full path - /path/to/downloads ): " dldirectory
-read -r -p "Where do you store your TV media? (Please use full path - /path/to/tv ): " tvdirectory
-read -r -p "Where do you store your MOVIE media? (Please use full path - /path/to/movies ): " moviedirectory
-read -r -p "Where do you store your MUSIC media? (Please use full path - /path/to/music ): " musicdirectory
+read -r -p "Where do store your TV media? (Please use full path - /path/to/tv ): " tvdirectory
+read -r -p "Where do store your MOVIE media? (Please use full path - /path/to/movies ): " moviedirectory
+read -r -p "Where do store your MUSIC media? (Please use full path - /path/to/music ): " musicdirectory
 fi
 # Create the directory structure
-if [ -z "$dldirectory" ]; then
-    mkdir -p content/completed
-    mkdir -p content/incomplete
-    dldirectory="$PWD/content"
-else
-  mkdir -p "$dldirectory"/completed
-  mkdir -p "$dldirectory"/incomplete
-fi
 if [ -z "$tvdirectory" ]; then
     mkdir -p content/tv
     tvdirectory="$PWD/content/tv"
@@ -161,15 +149,15 @@ if [ -z "$musicdirectory" ]; then
     mkdir -p content/music
     musicdirectory="$PWD/content/music"
 fi
+mkdir -p content/completed
+mkdir -p content/incomplete
 mkdir -p couchpotato
 mkdir -p delugevpn
 mkdir -p delugevpn/config/openvpn
 mkdir -p duplicati
 mkdir -p duplicati/backups
 mkdir -p headphones
-mkdir -p historical
 mkdir -p jackett
-mkdir -p lidarr
 mkdir -p minio
 mkdir -p muximux
 mkdir -p nzbget
@@ -203,7 +191,7 @@ do
     # it'll ask for another unless we leave the loop
     break
 done
-# TODO - Add a default server selection if none selected ..
+# TODO - Add a default server selection if none selected .. 
 cp ovpn/*.crt delugevpn/config/openvpn/ > /dev/null 2>&1
 cp ovpn/*.pem delugevpn/config/openvpn/ > /dev/null 2>&1
 
@@ -221,26 +209,23 @@ cat << EOF > .env
 ###  -----------------------------------------------
 ###
 EOF
-{
-echo "LOCALUSER=$localuname"
-echo "HOSTNAME=$thishost"
-echo "IP_ADDRESS=$locip"
-echo "PUID=$PUID"
-echo "PGID=$PGID"
-echo "PWD=$PWD"
-echo "DLDIR=$dldirectory"
-echo "TVDIR=$tvdirectory"
-echo "MOVIEDIR=$moviedirectory"
-echo "MUSICDIR=$musicdirectory"
-echo "PIAUNAME=$piauname"
-echo "PIAPASS=$piapass"
-echo "CIDR_ADDRESS=$lannet"
-echo "TZ=$time_zone"
-echo "PMSTAG=$pmstag"
-echo "PMSTOKEN=$pmstoken"
-echo "PORTAINERSTYLE=$portainerstyle"
-echo "VPN_REMOTE=$vpnremote"
-} >> .env
+echo "LOCALUSER=$localuname" >> .env
+echo "HOSTNAME=$thishost" >> .env
+echo "IP_ADDRESS=$locip" >> .env
+echo "PUID=$PUID" >> .env
+echo "PGID=$PGID" >> .env
+echo "PWD=$PWD" >> .env
+echo "TVDIR=$tvdirectory" >> .env
+echo "MOVIEDIR=$moviedirectory" >> .env
+echo "MUSICDIR=$musicdirectory" >> .env
+echo "PIAUNAME=$piauname" >> .env
+echo "PIAPASS=$piapass" >> .env
+echo "CIDR_ADDRESS=$lannet" >> .env
+echo "TZ=$time_zone" >> .env
+echo "PMSTAG=$pmstag" >> .env
+echo "PMSTOKEN=$pmstoken" >> .env
+echo "PORTAINERSTYLE=$portainerstyle" >> .env
+echo "VPN_REMOTE=$vpnremote" >> .env
 echo ".env file creation complete"
 printf "\\n\\n"
 
@@ -248,9 +233,6 @@ printf "\\n\\n"
 docker rm -f plexpy > /dev/null 2>&1
 # Adjust for old uhttpd web container - Noted in issue #47
 docker rm -f uhttpd > /dev/null 2>&1
-[ -d "www/" ] && mv www/ historical/www/
-# Move back-up .env files
-mv 20*.env historical/ > /dev/null 2>&1
 
 # Download & Launch the containers
 echo "The containers will now be pulled and launched"
@@ -262,7 +244,7 @@ printf "\\n\\n"
 
 # Configure the access to the Deluge Daemon
 # The same credentials can be used for NZBGet's webui
-if [ -z "$daemonun" ]; then
+if [ -z "$daemonun" ]; then 
 echo "You need to set a username and password for programs to access"
 echo "The Deluge daemon and NZBGet's API and web interface."
 read -r -p "What would you like to use as the access username?: " daemonun
@@ -291,32 +273,30 @@ docker start nzbget > /dev/null 2>&1
 
 # Push the Deluge Daemon and NZBGet Access info the to Auth file - and to the .env file
 echo "$daemonun":"$daemonpass":10 >> ./delugevpn/config/auth
-{
-echo "CPDAEMONUN=$daemonun"
-echo "CPDAEMONPASS=$daemonpass"
-echo "NZBGETUN=$daemonun"
-echo "NZBGETPASS=$daemonpass"
-} >> .env
+echo "CPDAEMONUN=$daemonun" >> .env
+echo "CPDAEMONPASS=$daemonpass" >> .env
+echo "NZBGETUN=$daemonun" >> .env
+echo "NZBGETPASS=$daemonpass" >> .env
+
 # Configure Muximux settings and files
 while [ ! -f muximux/www/muximux/settings.ini.php-example ]; do sleep 1; done
 docker stop muximux > /dev/null 2>&1
 cp settings.ini.php muximux/www/muximux/settings.ini.php
 cp mediaboxconfig.php muximux/www/muximux/mediaboxconfig.php
-sed '/^PIA/d' < .env > muximux/www/muximux/env.txt # Pull PIA creds from the displayed .env file
+cp .env muximux/www/muximux/env.txt
 perl -i -pe "s/locip/$locip/g" muximux/www/muximux/settings.ini.php
 perl -i -pe "s/locip/$locip/g" muximux/www/muximux/mediaboxconfig.php
 perl -i -pe "s/daemonun/$daemonun/g" muximux/www/muximux/mediaboxconfig.php
 perl -i -pe "s/daemonpass/$daemonpass/g" muximux/www/muximux/mediaboxconfig.php
 docker start muximux > /dev/null 2>&1
 
-# If PlexPy existed - copy plexpy.db to Tautulli
+# If PlexPy existed - copy plexpy.db to Tautulli 
 if [ -e plexpy/plexpy.db ]; then
     docker stop tautulli > /dev/null 2>&1
     mv tautulli/tautulli.db tautulli/tautulli.db.orig
     cp plexpy/plexpy.db tautulli/tautulli.db
     mv plexpy/plexpy.db plexpy/plexpy.db.moved
     docker start tautulli > /dev/null 2>&1
-    mv plexpy/ historical/plexpy/
 fi
 
 # Fix the Healthcheck in Minio
@@ -326,5 +306,5 @@ docker exec minio sed -i "s/404/403/g" /usr/bin/healthcheck.sh
 chmod -R 0777 content/
 
 printf "Setup Complete - Open a browser and go to: \\n\\n"
-printf "http://%s \\nOR http://%s If you have appropriate DNS configured.\\n\\n" "$locip" "$thishost"
+printf "http://$locip \\nOR http://$thishost If you have appropriate DNS configured.\\n\\n"
 printf "Start with the MEDIABOX Icon for settings and configuration info.\\n"
